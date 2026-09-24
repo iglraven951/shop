ed31885f931cb3d65d793b04c619a90b0260f0ca
Edwin Tataje <74156177@certus.edu.pe>
2026-09-24 01:03:58 -0500

    Delete android directory

--- android/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Proyecto raíz: solo declara los plugins. Toda la configuración vive en :app.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-}
